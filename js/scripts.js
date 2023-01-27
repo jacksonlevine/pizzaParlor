3f631b6189851e04ba4ec6508a3a68cfa06f7c5b
jacksonlevine <jack@netbug.us>
2023-01-27 08:38:25 -0800

    write Pizza.calculateCost() for Pizza prototype (test passed!)

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.calculateCost = function() {
+  let prices = {
+    large: 15,
+    medium: 10,
+    small: 5
+  }
+  let cost = prices[this.size] + this.toppings.length;
+  return "$" + cost;
+}
