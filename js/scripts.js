e433ce90908edd6e75a26ece10eebfbd922faee7
jacksonlevine <jack@netbug.us>
2023-01-27 09:31:30 -0800

    WIP: add functions to receive form and compile choices

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,46 @@ Pizza.prototype.calculateCost = function() {
   let cost = prices[this.size] + this.toppings.length;
   return "$" + cost;
 }
+
+// User interface logic
+
+onload = myLoadFunction
+
+function myLoadFunction() {
+  let form = document.querySelector("form");
+  form.onsubmit = receiveForm
+}
+
+function receiveForm(event) {
+  event.preventDefault();
+  let sizeChoice = getRadioInput("pizza_size");
+  let toppingsChoices = getCheckboxInputs("toppings");
+  if(sizeChoice !== false) {
+    
+  }
+}
+
+function getRadioInput(radioGroupName) {
+  let radios = document.getElementsByName(radioGroupName);
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
+  }
+  return false;
+}
+
+function getCheckboxInputs(checkGroupName) {
+  let checkBoxes = document.getElementsByName(checkGroupName);
+  let arrayOfChoices = [];
+  for(let i = 0; i < checkBoxes.length; i++) {
+    if(checkBoxes[i].checked) {
+      arrayOfChoices.push(checkBoxes[i].value);
+    }
+  }
+  if(arrayOfChoices.length > 0) {
+    return arrayOfChoices;
+  } else {
+    return false;
+  }
+}
